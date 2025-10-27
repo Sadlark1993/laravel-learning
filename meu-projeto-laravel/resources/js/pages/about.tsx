d4f83378bfb9abba749ce4c2ed828ca704b57de5
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-10-27 07:42:54 -0400

    first commit

---
+++ meu-projeto-laravel/resources/js/pages/about.tsx
@@ -0,0 +1,10 @@
+import { Head } from '@inertiajs/react';
+
+export default function About() {
+    return (
+        <>
+            <Head></Head>
+            <h1>About Page</h1>
+        </>
+    );
+}
