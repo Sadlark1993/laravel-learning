259a341293f2f6a801f5b86ac8116e0f2d22a022
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-11-01 20:53:53 -0400

    factories

--- meu-projeto-laravel/app/Models/Job.php
+++ meu-projeto-laravel/app/Models/Job.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Job extends Model
 {
+    use HasFactory, Notifiable;
+
     // setting the name of the table that this model represents
     protected $table = 'job_listings';
 
